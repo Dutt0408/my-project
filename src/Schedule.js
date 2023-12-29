5da62114c233a88963958424994d9c347d6e9cb3
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-29 16:44:14 -0500

    Commit yuplo

--- src/Schedule.js
+++ src/Schedule.js
@@ -6,8 +6,8 @@ import './index.css'
 const scheduleData = {
   
   "friday": [
-    { "Match": 1, "Team1": "ETOBICOKE KINGS", "Team2": "BRAMPTON LIONS", "TimeFrame": "2pm - 3pm","Day": "Friday","isOver": true,"ResultURL": "Result1.png","Winner":"Brampton Lions Won the match" },
-    { "Match": 2, "Team1": "BRAMPTON PANTHERS", "Team2": "ETOBICOKE EAGLES", "TimeFrame": "3pm-4pm","Day": "Friday","isOver": true,"ResultURL": "Result2.png","Winner":"" },
+    { "Match": 1, "Team1": "ETOBICOKE KINGS", "Team2": "BRAMPTON LIONS", "TimeFrame": "2pm - 3pm","Day": "Friday","isOver": true,"ResultURL": "Result1.png","Winner":"Etobicoke Kings Won the match" },
+    { "Match": 2, "Team1": "BRAMPTON PANTHERS", "Team2": "ETOBICOKE EAGLES", "TimeFrame": "3pm-4pm","Day": "Friday","isOver": true,"ResultURL": "Result2.png","Winner":"Etobicoke Eagles Won the match" },
     { "Match": 3, "Team1": "ETOBICOKE KINGS", "Team2": "RCB", "TimeFrame": "4pm-5pm","Day": "Friday","isOver": false,"ResultURL": "Result3.png","Winner":"" },
     { "Match": 4, "Team1": "ETOBICOKE TITANS", "Team2": "SCARBOROUGH SPARTANS", "TimeFrame": "5pm-6pm","Day": "Friday","isOver": false,"ResultURL": "Result4.png","Winner":""},
     { "Match": 5, "Team1": "BRAMPTON TIGERS", "Team2": "SCARBOROUGH WARRIORS", "TimeFrame": "6pm-7pm","Day": "Friday","isOver": false,"ResultURL": "Result5.png","Winner":"" },
