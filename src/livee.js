2fbc7d0cf46ed1096af0830fd708d27bda4349e5
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-30 13:36:03 -0500

    Commit op

--- src/livee.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Live() {
-  return (
-    <div className='text-white text-3xl'>live IS DISABLED For This Match See you soon !!!1</div>
-  );
-}
-
-export default Live;
---
+++ src/lives.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import "./Live.css"
+
+function live() {
+  return (
+    <div className='Livesection'>
+              
+<div className="wontoss">S.Warriors Choose to Bat !! </div>
+
+
+
+
+<div className="teamlo">
+
+<div className="team10">
+
+<div className="teamname">S.Warriors</div> 
+<div className="scorer">
+<div className="scorelive">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+
+
+<div className="team10">
+<div className="circle pos1"></div>
+<div className="teamname">K.TITANS</div> 
+<div className="scorer">
+<div className="score">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+
+
+<div className="overnews"> Update/News: </div>
+<div className="overnewss">   </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="next">Upcoming: </div>
+<div className="wontoss"> S.Warriors Choose to Bat !!</div>
+
+
+<div className="teamlo">
+
+<div className="team10">
+
+<div className="teamname">L.Strikers</div> 
+<div className="scorer">
+<div className="scorelive">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+
+
+<div className="team10">
+<div className="circle pos1"></div>
+<div className="teamname">S.Strikers</div> 
+<div className="scorer">
+<div className="score">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+</div> */}
+
+    </div>
+  )
+}
+
+export default live
