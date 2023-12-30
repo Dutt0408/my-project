6afa56a71124c9524341f6fdedbcfb6083522ede
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-30 16:25:37 -0500

    Commit 90

--- src/live.js
+++ src/live.js
@@ -16,8 +16,8 @@ function live() {
 
 <div className="teamname">S.Sparatans</div> 
 <div className="scorer">
-<div className="scorelive">0/0</div>
-<div className="Over">(0)</div>
+<div className="scorelive">93/5</div>
+<div className="Over">(10)</div>
 </div>
 </div>
 
