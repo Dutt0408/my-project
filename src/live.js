570a121b29fc40bd60557e4c06d840026ffe82a9
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-30 11:49:18 -0500

    Commit 19

--- src/live.js
+++ src/live.js
@@ -24,14 +24,14 @@ function live() {
 <div className="circle pos1"></div>
 <div className="teamname">L.Strikers</div> 
 <div className="scorer">
-<div className="score">0/0</div>
-<div className="Over">(0)</div>
+<div className="score">8/1</div>
+<div className="Over">(1)</div>
 </div>
 </div>
 
 
 <div className="overnews"> Update/News: </div>
-<div className="overnewss"> 60 to Chase!!! </div>
+<div className="overnewss"> Rajdeep and Chaitanya on Bat !!! </div>
 {/* <div className="overnewsss"> Dipesh on Bowl!!! </div>  */}
 </div>
 
