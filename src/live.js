beffc3747b94466392f261bc69c2d5ba17d9b113
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-30 10:15:37 -0500

    Commit cwbshbwshdd

--- src/live.js
+++ src/live.js
@@ -5,7 +5,7 @@ function live() {
   return (
     <div className='Livesection'>
               
-<div className="wontoss"> 46 to win from 46</div>
+<div className="wontoss"> 16 to win from 18</div>
 
 
 <div className="teamlo">
@@ -24,13 +24,13 @@ function live() {
 <div className="circle pos1"></div>
 <div className="teamname">S.Falcons</div> 
 <div className="scorer">
-<div className="score">47/4</div>
-<div className="Over">(6)</div>
+<div className="score">53/4</div>
+<div className="Over">(7)</div>
 </div>
 </div>
 </div>
 <div className="next">Upcoming </div>
-<div className="wontoss"> E.Capitals Choose to Bowling !!</div>
+<div className="wontoss"> E.Capitals Choose to Bowl !!</div>
 
 
 <div className="teamlo">
