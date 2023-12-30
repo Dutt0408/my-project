50c397a9677bc150cf56dd5cf28a65c71293f745
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-30 10:28:12 -0500

    Commit bvbshbvs

--- src/live.js
+++ src/live.js
@@ -5,51 +5,14 @@ function live() {
   return (
     <div className='Livesection'>
               
-<div className="wontoss"> 4 to win from 12</div>
+<div className="wontoss">E.Capitals Choose to Bowl !! </div>
 
 
 <div className="teamlo">
 
 <div className="team10">
 
-<div className="teamname">K.Titans</div> 
-<div className="scorer">
-<div className="scorelive">68/6</div>
-<div className="Over">(10)</div>
-</div>
-</div>
-
-
-<div className="team10">
-<div className="circle pos1"></div>
-<div className="teamname">S.Falcons</div> 
-<div className="scorer">
-<div className="score">69/5</div>
-<div className="Over">(8.1)</div>
-</div>
-</div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-<div className="next">Upcoming: </div>
-<div className="wontoss"> E.Capitals Choose to Bowl !!</div>
-
-
-<div className="teamlo">
-
-<div className="team10">
-
-<div className="teamname">RCH</div> 
+<div className="teamname">E.Capitals</div> 
 <div className="scorer">
 <div className="scorelive">0/0</div>
 <div className="Over">(0)</div>
@@ -59,13 +22,50 @@ function live() {
 
 <div className="team10">
 <div className="circle pos1"></div>
-<div className="teamname">E.Capitals</div> 
+<div className="teamname">RCH</div> 
 <div className="scorer">
 <div className="score">0/0</div>
 <div className="Over">(0)</div>
 </div>
 </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="next">Upcoming: </div>
+<div className="wontoss"> E.Capitals Choose to Bowl !!</div>
+
+
+<div className="teamlo">
+
+<div className="team10">
+
+<div className="teamname"></div> 
+<div className="scorer">
+<div className="scorelive">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+
+
+<div className="team10">
+<div className="circle pos1"></div>
+<div className="teamname"></div> 
+<div className="scorer">
+<div className="score">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+</div> */}
 
     </div>
   )
