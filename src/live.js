4a6427bf6f22c6f6392a36cc4550c0f53b487764
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-31 12:31:10 -0500

    Commit hfvfdhqr.1.8

--- src/live.js
+++ src/live.js
@@ -16,8 +16,8 @@ function live() {
 
 <div className="teamname">E.Kings</div> 
 <div className="scorer">
-<div className="scorelive">95/1</div>
-<div className="Over">(9)</div>
+<div className="scorelive">99/1</div>
+<div className="Over">(9.1)</div>
 </div>
 </div>
 
