a8783643cb0ff024abd55b7a5a4c84c479585336
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-31 01:38:35 -0500

    Commit 101010

--- src/Schedule.js
+++ src/Schedule.js
@@ -5,43 +5,45 @@ import './index.css'
 
 const scheduleData = {
   
-  "friday": [
-    { "Match": 1, "Team1": "ETOBICOKE KINGS", "Team2": "BRAMPTON LIONS", "TimeFrame": "2pm - 3pm","Day": "Friday","isOver": true,"ResultURL": "Result1.png","Winner":"Etobicoke Kings won!!" },
-    { "Match": 2, "Team1": "BRAMPTON PANTHERS", "Team2": "ETOBICOKE EAGLES", "TimeFrame": "3pm-4pm","Day": "Friday","isOver": true,"ResultURL": "Result2.png","Winner":"Etobicoke Eagles  won!!" },
-    { "Match": 3, "Team1": "ETOBICOKE KINGS", "Team2": "Royal Challangers.B", "TimeFrame": "4pm-5pm","Day": "Friday","isOver": true,"ResultURL": "Result3.png","Winner":"ETOBICOKE KINGS  won!!" },
-    { "Match": 4, "Team1": "ETOBICOKE TITANS", "Team2": "S. SPARTANS", "TimeFrame": "5pm-6pm","Day": "Friday","isOver": true,"ResultURL": "Result4.png","Winner":"ETOBICOKE TITANS  won!!"},
-    { "Match": 5, "Team1": "BRAMPTON TIGERS", "Team2": "S.WARRIORS", "TimeFrame": "6pm-7pm","Day": "Friday","isOver": true,"ResultURL": "Result5.png","Winner":"BRAMPTON TIGERS  won!!" },
-    { "Match": 6, "Team1": "Royal Challangers.B", "Team2": "ETOBICOKE CAPITALS", "TimeFrame": "7pm-8pm","Day": "Friday","isOver": true,"ResultURL": "Result6.png","Winner":"Royal Challangers.B  won!!" },
-    { "Match": 7, "Team1": "S.FALCONS", "Team2": "BRAMPTON PANTHERS", "TimeFrame": "8pm-9pm","Day": "Friday","isOver": true,"ResultURL": "Result7.png","Winner":"BRAMPTON PANTHERS  won!!" },
-    { "Match": 8, "Team1": "BRAMPTON LIONS", "Team2": "S.SPARTANS", "TimeFrame": "9pm-10pm","Day": "Friday","isOver": true,"ResultURL": "Result8.png","Winner":"BRAMPTON LIONS  won!!" },
-    { "Match": 9, "Team1": "S.STRIKERS", "Team2": "ETOBICOKE EAGLES", "TimeFrame": "10pm-11pm","Day": "Friday","isOver": true,"ResultURL": "Result9.png","Winner":"ETOBICOKE EAGLES  won!!" }
-  ],
-  "saturday": [
-    { "Match": 10, "Team1": "C.SUPER KINGS", "Team2": "ETOBICOKE TITANS", "TimeFrame": "7am-8am","Day": "Saturday","isOver": true,"ResultURL": "Result10.png","Winner":"ETOBICOKE EAGLES  won!!" },
-    { "Match": 11, "Team1": "BRAMPTON TIGERS", "Team2": "LONDON STRIKERS", "TimeFrame": "8am-9am","Day": "Saturday","isOver": true,"ResultURL": "Result11.png","Winner":"BRAMPTON TIGERS won!!" },
-    { "Match": 12, "Team1": "KITCHNER TITANS", "Team2": "S.FALCONS", "TimeFrame": "9am-10am","Day": "Saturday","isOver": true,"ResultURL": "Result12.png","Winner":"S.FALCONS won!!!" },
-    { "Match": 13, "Team1": "Royal Challangers.H", "Team2": "ETOBICOKE CAPITALS", "TimeFrame": "10am-11pm","Day": "Saturday","isOver": true,"ResultURL": "Result13.png","Winner":"RCH won!!" },
-    { "Match": 14, "Team1": "LONDON STRIKERS", "Team2": "S.STRIKERS", "TimeFrame": "11am-12pm","Day": "Saturday","isOver": true,"ResultURL": "Result14.png" ,"Winner":"LONDON STRIKERS won!!"},
-    { "Match": 15, "Team1": "C. SUPER KINGS", "Team2": "Royal Challangers.B", "TimeFrame": "12pm-1pm","Day": "Saturday","isOver": true,"ResultURL": "Result15.png","Winner":"C. SUPER KINGS won!!" },
-    { "Match": 16, "Team1": "KITCHNER TITANS", "Team2": "S.WARRIORS", "TimeFrame": "1pm-2pm","Day": "Saturday","isOver": true,"ResultURL": "Result16.png","Winner":"KITCHNER TITANS won!!!" },
-    { "Match": 17, "Team1": "Royal Challangers.H", "Team2": "BRAMPTON LIONS", "TimeFrame": "2pm-3pm","Day": "Saturday","isOver": true,"ResultURL": "Result17.png","Winner":"BRAMPTON LIONS won!!" },
-    { "Match": 18, "Team1": "LONDON STRIKERS", "Team2": "ETOBICOKE EAGLES", "TimeFrame": "3pm-4pm","Day": "Saturday","isOver": true,"ResultURL": "Result18.png","Winner":"ETOBICOKE EAGLES won!!" },
-    { "Match": 19, "Team1": "S.SPARTANS", "Team2": "C. SUPER KINGS", "TimeFrame": "4pm-5pm","Day": "Saturday","isOver": true,"ResultURL": "Result19.png","Winner":"C.SK won!!" },
-    { "Match": 20, "Team1": "S.STRIKERS", "Team2": "BRAMPTON PANTHERS", "TimeFrame": "5pm-6pm","Day": "Saturday","isOver": true,"ResultURL": "Result20.png","Winner":"S.Strikers won!!" },
-    { "Match": 21, "Team1": "ETOBICOKE KINGS", "Team2": "Royal Challangers.H", "TimeFrame": "6pm-7pm","Day": "Saturday","isOver":true,"ResultURL": "Result21.png","Winner":"ETOBICOKE KINGS won!!" },
-    { "Match": 22, "Team1": "KITCHNER TITANS", "Team2": "BRAMPTON TIGERS", "TimeFrame": "7pm-8pm","Day": "Saturday","isOver": true,"ResultURL": "Result22.png","Winner":"KITCHNER TITANS won!!" },
-    { "Match": 23, "Team1": "ETOBICOKE TITANS", "Team2": "ETOBICOKE CAPITALS", "TimeFrame": "8pm-9pm","Day": "Saturday","isOver": true,"ResultURL": "Result23.png","Winner":"ETOBICOKE TITANS won!!" },
-    { "Match": 24, "Team1": "S.WARRIORS", "Team2": "S.FALCONS", "TimeFrame": "9pm-10pm","Day": "Saturday","isOver": true,"ResultURL": "Result24.png","Winner":"S.FALCONS won!!" }
-  ],
+ 
   "sunday": [
-    { "Match": 25, "Team1": "QF1--1", "Team2": "QF1--2", "TimeFrame": "7am-8am","Day": "Sunday","isOver": false,"ResultURL": "Result25.png","Winner":"" },
-    { "Match": 26, "Team1": "QF2--2", "Team2": "QF2--2", "TimeFrame": "8am-9am","Day": "Sunday","isOver": false,"ResultURL": "Result26.png","Winner":"" },
-    { "Match": 27, "Team1": "QF3--1", "Team2": "QF3--2", "TimeFrame": "9am-10am","Day": "Sunday","isOver": false,"ResultURL": "Result27.png","Winner":"" },
-    { "Match": 28, "Team1": "QF4--1", "Team2": "QF4--2", "TimeFrame": "10am-11am","Day": "Sunday","isOver": false,"ResultURL": "Result28.png","Winner":"" },
+    { "Match": 25, "Team1": "E.Kings", "Team2": "B.Lions", "TimeFrame": "7am-8am","Day": "Sunday","isOver": false,"ResultURL": "Result25.png","Winner":"" },
+    { "Match": 26, "Team1": "E.Titans", "Team2": "K.Titans", "TimeFrame": "8am-9am","Day": "Sunday","isOver": false,"ResultURL": "Result26.png","Winner":"" },
+    { "Match": 27, "Team1": "E.Eagles", "Team2": "C.SuperKings", "TimeFrame": "9am-10am","Day": "Sunday","isOver": false,"ResultURL": "Result27.png","Winner":"" },
+    { "Match": 28, "Team1": "B.Tigers", "Team2": "S.Falcons", "TimeFrame": "10am-11am","Day": "Sunday","isOver": false,"ResultURL": "Result28.png","Winner":"" },
     { "Match": 29, "Team1": "SEMI FINAL 1", "Team2": "-", "TimeFrame": "11am-12pm","Day": "Sunday","isOver": false,"ResultURL": "Result29.png","Winner":"" },
     { "Match": 30, "Team1": "SEMI FINAL 2", "Team2": "-", "TimeFrame": "12pm-1pm","Day": "Sunday","isOver": false,"ResultURL": "Result30.png","Winner":"" },
     { "Match": 31, "Team1": "Final", "Team2": "-", "TimeFrame": "1pm-2pm","Day": "Sunday","isOver": false,"ResultURL": "Result31.png","Winner":"" }
   ]
+,
+"friday": [
+  { "Match": 1, "Team1": "ETOBICOKE KINGS", "Team2": "BRAMPTON LIONS", "TimeFrame": "2pm - 3pm","Day": "Friday","isOver": true,"ResultURL": "Result1.png","Winner":"Etobicoke Kings won!!" },
+  { "Match": 2, "Team1": "BRAMPTON PANTHERS", "Team2": "ETOBICOKE EAGLES", "TimeFrame": "3pm-4pm","Day": "Friday","isOver": true,"ResultURL": "Result2.png","Winner":"Etobicoke Eagles  won!!" },
+  { "Match": 3, "Team1": "ETOBICOKE KINGS", "Team2": "Royal Challangers.B", "TimeFrame": "4pm-5pm","Day": "Friday","isOver": true,"ResultURL": "Result3.png","Winner":"ETOBICOKE KINGS  won!!" },
+  { "Match": 4, "Team1": "ETOBICOKE TITANS", "Team2": "S. SPARTANS", "TimeFrame": "5pm-6pm","Day": "Friday","isOver": true,"ResultURL": "Result4.png","Winner":"ETOBICOKE TITANS  won!!"},
+  { "Match": 5, "Team1": "BRAMPTON TIGERS", "Team2": "S.WARRIORS", "TimeFrame": "6pm-7pm","Day": "Friday","isOver": true,"ResultURL": "Result5.png","Winner":"BRAMPTON TIGERS  won!!" },
+  { "Match": 6, "Team1": "Royal Challangers.B", "Team2": "ETOBICOKE CAPITALS", "TimeFrame": "7pm-8pm","Day": "Friday","isOver": true,"ResultURL": "Result6.png","Winner":"Royal Challangers.B  won!!" },
+  { "Match": 7, "Team1": "S.FALCONS", "Team2": "BRAMPTON PANTHERS", "TimeFrame": "8pm-9pm","Day": "Friday","isOver": true,"ResultURL": "Result7.png","Winner":"BRAMPTON PANTHERS  won!!" },
+  { "Match": 8, "Team1": "BRAMPTON LIONS", "Team2": "S.SPARTANS", "TimeFrame": "9pm-10pm","Day": "Friday","isOver": true,"ResultURL": "Result8.png","Winner":"BRAMPTON LIONS  won!!" },
+  { "Match": 9, "Team1": "S.STRIKERS", "Team2": "ETOBICOKE EAGLES", "TimeFrame": "10pm-11pm","Day": "Friday","isOver": true,"ResultURL": "Result9.png","Winner":"ETOBICOKE EAGLES  won!!" }
+],
+"saturday": [
+  { "Match": 10, "Team1": "C.SUPER KINGS", "Team2": "ETOBICOKE TITANS", "TimeFrame": "7am-8am","Day": "Saturday","isOver": true,"ResultURL": "Result10.png","Winner":"ETOBICOKE EAGLES  won!!" },
+  { "Match": 11, "Team1": "BRAMPTON TIGERS", "Team2": "LONDON STRIKERS", "TimeFrame": "8am-9am","Day": "Saturday","isOver": true,"ResultURL": "Result11.png","Winner":"BRAMPTON TIGERS won!!" },
+  { "Match": 12, "Team1": "KITCHNER TITANS", "Team2": "S.FALCONS", "TimeFrame": "9am-10am","Day": "Saturday","isOver": true,"ResultURL": "Result12.png","Winner":"S.FALCONS won!!!" },
+  { "Match": 13, "Team1": "Royal Challangers.H", "Team2": "ETOBICOKE CAPITALS", "TimeFrame": "10am-11pm","Day": "Saturday","isOver": true,"ResultURL": "Result13.png","Winner":"RCH won!!" },
+  { "Match": 14, "Team1": "LONDON STRIKERS", "Team2": "S.STRIKERS", "TimeFrame": "11am-12pm","Day": "Saturday","isOver": true,"ResultURL": "Result14.png" ,"Winner":"LONDON STRIKERS won!!"},
+  { "Match": 15, "Team1": "C. SUPER KINGS", "Team2": "Royal Challangers.B", "TimeFrame": "12pm-1pm","Day": "Saturday","isOver": true,"ResultURL": "Result15.png","Winner":"C. SUPER KINGS won!!" },
+  { "Match": 16, "Team1": "KITCHNER TITANS", "Team2": "S.WARRIORS", "TimeFrame": "1pm-2pm","Day": "Saturday","isOver": true,"ResultURL": "Result16.png","Winner":"KITCHNER TITANS won!!!" },
+  { "Match": 17, "Team1": "Royal Challangers.H", "Team2": "BRAMPTON LIONS", "TimeFrame": "2pm-3pm","Day": "Saturday","isOver": true,"ResultURL": "Result17.png","Winner":"BRAMPTON LIONS won!!" },
+  { "Match": 18, "Team1": "LONDON STRIKERS", "Team2": "ETOBICOKE EAGLES", "TimeFrame": "3pm-4pm","Day": "Saturday","isOver": true,"ResultURL": "Result18.png","Winner":"ETOBICOKE EAGLES won!!" },
+  { "Match": 19, "Team1": "S.SPARTANS", "Team2": "C. SUPER KINGS", "TimeFrame": "4pm-5pm","Day": "Saturday","isOver": true,"ResultURL": "Result19.png","Winner":"C.SK won!!" },
+  { "Match": 20, "Team1": "S.STRIKERS", "Team2": "BRAMPTON PANTHERS", "TimeFrame": "5pm-6pm","Day": "Saturday","isOver": true,"ResultURL": "Result20.png","Winner":"S.Strikers won!!" },
+  { "Match": 21, "Team1": "ETOBICOKE KINGS", "Team2": "Royal Challangers.H", "TimeFrame": "6pm-7pm","Day": "Saturday","isOver":true,"ResultURL": "Result21.png","Winner":"ETOBICOKE KINGS won!!" },
+  { "Match": 22, "Team1": "KITCHNER TITANS", "Team2": "BRAMPTON TIGERS", "TimeFrame": "7pm-8pm","Day": "Saturday","isOver": true,"ResultURL": "Result22.png","Winner":"KITCHNER TITANS won!!" },
+  { "Match": 23, "Team1": "ETOBICOKE TITANS", "Team2": "ETOBICOKE CAPITALS", "TimeFrame": "8pm-9pm","Day": "Saturday","isOver": true,"ResultURL": "Result23.png","Winner":"ETOBICOKE TITANS won!!" },
+  { "Match": 24, "Team1": "S.WARRIORS", "Team2": "S.FALCONS", "TimeFrame": "9pm-10pm","Day": "Saturday","isOver": true,"ResultURL": "Result24.png","Winner":"S.FALCONS won!!" }
+],
 };
 
 
@@ -116,7 +118,7 @@ const handleCloseModal = () => {
 
 return (
   <section className="design-section">
-    <h2 className="day-heading Schl">Schedule</h2>
+    <h2 className="day-heading Schl"> Final Day Schedule</h2>
     {Object.keys(scheduleData).map((day, index) => (
       <React.Fragment key={day}>
         {index < Object.keys(scheduleData).length - 1 && (
--- src/live.js
+++ src/live.js
@@ -1,91 +1,91 @@
-// import React from 'react'
-// import "./Live.css"
-
-// function live() {
-//   return (
-//     <div className='Livesection'>
-              
-// <div className="wontoss">S.Spartans Choose to Bat !! </div>
-
-
-
-
-// <div className="teamlo">
-
-// <div className="team10">
-
-// <div className="teamname">S.Sparatans</div> 
-// <div className="scorer">
-// <div className="scorelive">93/5</div>
-// <div className="Over">(10)</div>
-// </div>
-// </div>
-
-
-// <div className="team10">
-// <div className="circle pos1"></div>
-// <div className="teamname">CSK</div> 
-// <div className="scorer">
-// <div className="score">8/0</div>
-// <div className="Over">(1)</div>
-// </div>
-// </div>
-
-
-// <div className="overnews"> Update/News: </div>
-// <div className="overnewss"> CSK Need 91 to Win </div>
-
-// </div>
-
-
-
-
-
-
-
-
-
-
-
-
-// {/* <div className="next">Upcoming: </div>
-// <div className="wontoss"> S.Warriors Choose to Bat !!</div>
-
-
-// <div className="teamlo">
-
-// <div className="team10">
-
-// <div className="teamname">L.Strikers</div> 
-// <div className="scorer">
-// <div className="scorelive">0/0</div>
-// <div className="Over">(0)</div>
-// </div>
-// </div>
-
-
-// <div className="team10">
-// <div className="circle pos1"></div>
-// <div className="teamname">S.Strikers</div> 
-// <div className="scorer">
-// <div className="score">0/0</div>
-// <div className="Over">(0)</div>
-// </div>
-// </div>
-// </div> */}
-
-//     </div>
-//   )
-// }
-
-// export default live
-
 import React from 'react'
+import "./Live.css"
 
 function live() {
   return (
-    <div className='text-white text-3xl'>Live IS DISABLED For This Match See you soon !!!</div>
+    <div className='Livesection'>
+              
+<div className="wontoss">QF Match-1 </div>
+
+
+
+
+<div className="teamlo">
+
+<div className="team10">
+
+<div className="teamname">E.Kings</div> 
+<div className="scorer">
+<div className="scorelive">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+
+
+<div className="team10">
+<div className="circle pos1"></div>
+<div className="teamname">B.Lions</div> 
+<div className="scorer">
+<div className="score">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+
+
+<div className="overnews"> Update/News: </div>
+<div className="overnewss"> Live From 7AM </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="next">Upcoming: </div>
+<div className="wontoss"> S.Warriors Choose to Bat !!</div>
+
+
+<div className="teamlo">
+
+<div className="team10">
+
+<div className="teamname">L.Strikers</div> 
+<div className="scorer">
+<div className="scorelive">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+
+
+<div className="team10">
+<div className="circle pos1"></div>
+<div className="teamname">S.Strikers</div> 
+<div className="scorer">
+<div className="score">0/0</div>
+<div className="Over">(0)</div>
+</div>
+</div>
+</div> */}
+
+    </div>
   )
 }
 
 export default live
+
+// import React from 'react'
+
+// function live() {
+//   return (
+//     <div className='text-white text-3xl'>Live IS DISABLED For This Match See you soon !!!</div>
+//   )
+// }
+
+// export default live
