24f8fa886bf732f4f5db025e8a6768b1e06b0ed6
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-31 21:06:04 -0500

    Commit Final1000121.1

--- src/live.js
+++ src/live.js
@@ -16,8 +16,8 @@ function live() {
 
 <div className="teamname">E.Kings</div> 
 <div className="scorer">
-<div className="scorelive">85/6</div>
-<div className="Over">(10.2)</div>
+<div className="scorelive">89/6</div>
+<div className="Over">(10.4)</div>
 </div>
 </div>
 
