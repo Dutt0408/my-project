b33619f34448528686aa0b66fb9f25cfb2719d18
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-31 20:46:48 -0500

    Commit Finals1.4

--- src/live.js
+++ src/live.js
@@ -1,82 +1,46 @@
-import React from 'react'
-import "./Live.css"
+import React, { useState } from 'react';
+import "./Live.css";
 
-function live() {
+function Live() {
+  // Initialize state for scores
+  const [scores, setScores] = useState({
+    team1: { runs: 33, wickets: 1, overs: 3.5 },
+    team2: { runs: 0, wickets: 0, overs: 0 },
+  });
+
+  // Function to manually update scores
+  const manualUpdateScores = () => {
+    // Update the scores state
+    setScores({
+      team1: { runs: 5, wickets: 1, overs: 2 },
+      team2: { runs: 0, wickets: 0, overs: 0 },
+    });
+  };
+
   return (
     <div className='Livesection'>
-              
-<div className="wontoss">SEMI FINAL</div>
-
-
-
-
-<div className="teamlo">
-
-<div className="team10">
-
-<div className="teamname">E.Kings</div> 
-<div className="scorer">
-<div className="scorelive">13/1</div>
-<div className="Over">(2.4)</div>
-</div>
-</div>
-
-
-<div className="team10">
-{/* <div className="circle pos1"></div> */}
-<div className="teamname">E.Titans</div> 
-<div className="scorer">
-<div className="score">0/0</div>
-<div className="Over">(0)</div>
-</div>
-</div>
-
-
-<div className="overnews"> Live  </div>
-<div className="overnewss"> </div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <div className="next">Upcoming: </div>
-<div className="wontoss"> S.Warriors Choose to Bat !!</div>
-
-
-<div className="teamlo">
-
-<div className="team10">
-
-<div className="teamname">L.Strikers</div> 
-<div className="scorer">
-<div className="scorelive">0/0</div>
-<div className="Over">(0)</div>
-</div>
-</div>
-
-
-<div className="team10">
-<div className="circle pos1"></div>
-<div className="teamname">S.Strikers</div> 
-<div className="scorer">
-<div className="score">0/0</div>
-<div className="Over">(0)</div>
-</div>
-</div>
-</div> */}
-
+      <div className="wontoss">SEMI FINAL</div>
+      <div className="teamlo">
+        <div className="team10">
+          <div className="teamname">E.Kings</div>
+          <div className="scorer">
+            <div className="scorelive">{scores.team1.runs}/{scores.team1.wickets}</div>
+            <div className="Over">({scores.team1.overs})</div>
+          </div>
+        </div>
+        <div className="team10">
+          <div className="teamname">E.Titans</div>
+          <div className="scorer">
+            <div className="score">{scores.team2.runs}/{scores.team2.wickets}</div>
+            <div className="Over">({scores.team2.overs})</div>
+          </div>
+        </div>
+        <div className="overnews">Live</div>
+        <div className="overnewss"></div>
+        <button onClick={manualUpdateScores}></button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default live
-
+export default Live;
