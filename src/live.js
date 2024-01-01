946f13416397116407e0fa06a41727b36788fbc4
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2023-12-31 21:48:33 -0500

    Commit hgvsvcsgs

--- src/live.js
+++ src/live.js
@@ -1,46 +1,9 @@
-import React from 'react';
-import './Live.css';
+import React from 'react'
 
-function Live() {
+function live() {
   return (
-    <div className='Livesection'>
-      <div className="wontoss"> FINAL</div>
-      
-      <div className="teamlo">
-        <div className="team10">
-          <div className="teamname">E.Titans</div> 
-          <div className="scorer">
-            <div className="scorelive">Live On Video</div>
-            <div className="Over">(8)</div>
-          </div>
-        </div>
-  
-        <div className="team10">
-          <div className="teamname">E.Kings</div> 
-          <div className="scorer">
-            <div className="score">102/6</div>
-            <div className="Over">(12)</div>
-          </div>
-        </div>
-  
-        <div className="overnews"> Live  </div>
-        <div className="overnewss"> 103 to win !!! </div>
-      </div>
-      
-      {/* Updated YouTube video embed with autoplay and border */}
-      <div style={{ border: '5px solid #ccc', padding: '10px' }}>
-        <iframe 
-          
-          title="YouTube Video"
-          width="100%" 
-          height="315" 
-          src="https://www.youtube.com/embed/6n6Kz1FcDuQ?autoplay=1" 
-          frameBorder="0" 
-          allowFullScreen
-        ></iframe>
-      </div>
-    </div>
-  );
+    <div className='text-white text-3xl'> E.Kings Won the Final Tournament</div>
+  )
 }
 
-export default Live;
+export default live
