e409bd65c2658bad0597db91e1913b2ad87931cc
Duttbaps <dkeshavg1@gmail.com>
2025-01-25 04:56:51 -0500

    Commit Allpush

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import Navbar from "./Navbar";
 import Rules from "./Rules";
 import Home from "./Home";
-import Schedule from "./Schedule";
+// import Schedule from "./Schedule";
 import Teams from "./Teams";
 import PointTable from "./PointTable";
 import Sponsors from "./Sponsors"
+import Soon from "./Soon";
 
 // import { PrimeReactProvider } from "primereact/api";
 import { Routes, Route } from "react-router-dom";
@@ -20,10 +21,12 @@ function App() {
       <Route path="/" element={<Home />} />
         <Route path="/Rules" element={<Rules />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/Schedule" element={<Schedule />} />
+        {/* <Route path="/Schedule" element={<Schedule />} /> */}
+        <Route path="/Schedule" element={<Soon />} />
         <Route path="/Teams" element={<Teams />} />
         <Route path="/PointTable" element={<PointTable />} />
         <Route path="/Sponsors" element={<Sponsors/>}/>
+        <Route path="/Soon" element={<Soon/>}/>
        
       </Routes>
       {/* <Home/>
