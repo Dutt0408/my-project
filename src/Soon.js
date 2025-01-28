d4c12907cea6b16f0f35d737def674600969c6f3
Duttbaps <dkeshavg1@gmail.com>
2025-01-28 18:38:26 -0500

    Commit UnderMainatainance

--- src/Soon.js
+++ src/Soon.js
@@ -11,7 +11,7 @@ export default function Soon() {
 <div className="bg-gray-100">
   <div className="min-h-screen flex flex-col justify-center items-center">
     <img src= {image} alt="Logo" class="mb-8 h-40"/>
-    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 mb-4">Feature is under maintenance</h1>
+    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 mb-4">Website is under maintenance</h1>
     <p className="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">We're dedicated to enhancing user satisfaction. Keep an eye out for updates!</p>
   </div>
 </div>
